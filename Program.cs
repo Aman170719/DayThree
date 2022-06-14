479a4c51b6b077bc993a9c5666b1ffbb9820aa63
Aman <aman.kumar99499@gmail.com>
2022-06-14 08:47:33 +0530

    Files

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("welcone to the Salary Calculator Simulation");
+
+
+Random random = new Random();
+
+int num = random.Next(1, 3);
+
+Console.WriteLine(num);
+
+if (num == 1)
+{
+    Console.WriteLine("Present");
+
+
+}
+else
+{
+    Console.WriteLine("Absent");
+}
+
+
+
+
+
+
