440458a712396ae980c74ac34441fee25cac0875
Aman <aman.kumar99499@gmail.com>
2022-06-14 10:44:31 +0530

    Switch

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿Console.WriteLine("welcone to the Salary Calculator Simulation");
 
+const int Perhour = 20;
+const int Fullday = 8;
+const int PartTime = 4;
 
 Random random = new Random();
 
@@ -10,16 +13,21 @@ Console.WriteLine(num);
 if (num == 1)
 {
     Console.WriteLine("Present");
-
+    int salary = ( Perhour * Fullday );
+    Console.WriteLine( salary );
 
 }
 else
 {
     Console.WriteLine("Absent");
+    Console.WriteLine( "Noo wages" );
 }
 
-
-
+{
+    int PartTimeSalary = (PartTime * Perhour);
+    Console.WriteLine("The Part Time Salary is " + PartTimeSalary);
+    
+}
 
 
 
