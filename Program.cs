11d467b7411b694cec67cd02423d4ae3de1aa6d8
Aman <106363389+Aman170719@users.noreply.github.com>
2022-06-14 21:48:29 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,25 +1,37 @@
-﻿Console.WriteLine("welcone to the Salary Calculator Simulation");
-
-
+Console.WriteLine("welcone to the Salary Calculator Simulation");
 Random random = new Random();
 
-int num = random.Next(1, 3);
+const int Perhour = 20;
+const int Fullday = 8;
+const int PartTime = 4;
+const int workingday = 20;
 
+int num = random.Next(1, 3);
 Console.WriteLine(num);
 
 if (num == 1)
 {
     Console.WriteLine("Present");
-
+    int salary = (Perhour * Fullday);
+    Console.WriteLine(salary);
 
 }
 else
 {
     Console.WriteLine("Absent");
+    Console.WriteLine("Noo wages");
 }
 
+int PartSal = (PartTime * Perhour);
+Console.WriteLine("The Part Time Salary is : " + PartSal);
 
-
-
-
-
+{
+    int MonthlySalary = ( (Perhour * Fullday) * workingday );
+    
+    int sal = ( PartSal * Fullday);
+    
+    Console.WriteLine("Salary Per Month : " + MonthlySalary);
+    
+    Console.WriteLine("Salary Per Month as a Part Time Emp : " + sal);
+    
+}
